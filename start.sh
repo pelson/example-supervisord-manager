ff10077065a165423dbe8fa10831a8910fa6687e
Phil Elson <pelson.pub@gmail.com>
2017-08-01 07:16:31 +0100

    Be more explicit about the supervisor config being used.

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-SUPERVISOR_CONFIG_REPO_LOCN=$(pwd) supervisord 
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+export SUPERVISOR_CONFIG_REPO_LOCN=${DIR}
+
+cd ${SUPERVISOR_CONFIG_REPO_LOCN}
+supervisord -c ${SUPERVISOR_CONFIG_REPO_LOCN}/supervisord.conf
